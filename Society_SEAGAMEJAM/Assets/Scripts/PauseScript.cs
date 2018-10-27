93161f98cc9bcada328ca97462b00d4326dec534
KEBS\User <nubfight@gmail.com>
2018-10-27 15:44:17 +0800

    Dream Button, Work Button, Pause Script, Bar Fill, Win/Defeat Condition

---
+++ Society_SEAGAMEJAM/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    GameObject PausePanel;
+    public bool isPaused;
+
+    private void Start()
+    {
+        PausePanel = GameObject.FindGameObjectWithTag("PausePanel");
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if(isPaused)
+        {
+            PausePanel.SetActive(true);
+        }
+        else
+        {
+            PausePanel.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            Debug.Log("sad");
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {     
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;       
+        isPaused = false;
+    }
+}
