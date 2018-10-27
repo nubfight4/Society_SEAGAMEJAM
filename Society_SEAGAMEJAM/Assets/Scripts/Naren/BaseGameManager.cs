53639c585ccd829c6b64cf3af2d654f1a6007044
Narendra Arief N <narendra.arief.nugraha@gmail.com>
2018-10-27 15:36:00 +0700

    Naren Pull Master

---
+++ Society_SEAGAMEJAM/Assets/Scripts/Naren/BaseGameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Naren
+{
+    public class BaseGameManager : MonoBehaviour
+    {
+        public delegate void NextTurn();
+        public static event NextTurn OnNextTurn;
+        public static void DelegateNextTurn()
+        {
+            if (OnNextTurn != null)
+            {
+                DelegateNextTurn();
+            }
+        }
+
+    }
+}
---
+++ Society_SEAGAMEJAM/Assets/Scripts/Naren/UIGameplayManagerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Naren
+{
+    public class UIGameplayManagerBehaviour : MonoBehaviour
+    {
+        [SerializeField] Text moneyText;
+        int currentMoney = 0;
+
+        [SerializeField] Text timeText;
+        int currentMonth = 0;
+        [SerializeField] int maxMonthPerYear = 4;
+        int currentYear = 5;
+        [SerializeField] int deathYear = 68;
+
+        private void Start()
+        {
+            BaseGameManager.OnNextTurn += OnNextTurn;
+        }
+
+        private void Update()
+        {
+            moneyText.text = currentMoney + " Rupee";
+            timeText.text = (currentMonth * (12 / maxMonthPerYear)) + " month " + currentYear + " years";
+        }
+
+        private void OnNextTurn()
+        {
+
+        }
+    }
+}
