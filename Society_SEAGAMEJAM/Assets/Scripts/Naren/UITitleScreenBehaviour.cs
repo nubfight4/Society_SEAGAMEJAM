6eaaa777b88000f45f351bdbecf85fcaed2b2a1c
Narendra Arief N <narendra.arief.nugraha@gmail.com>
2018-10-28 16:57:16 +0700

    Title Screen, Credit, Win, Lose

---
+++ Society_SEAGAMEJAM/Assets/Scripts/Naren/UITitleScreenBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UITitleScreenBehaviour : MonoBehaviour {
+    public void ChangeScene(string gameplayScene)
+    {
+        SoundManagerScript.mInstance.PlaySFX(AudioClipID.SFX_MOUSE_CLICK);
+        StartCoroutine(ChangeSceneEnumerator(gameplayScene));
+    }
+
+    IEnumerator ChangeSceneEnumerator(string gameplayScene)
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(gameplayScene);
+    }
+}
